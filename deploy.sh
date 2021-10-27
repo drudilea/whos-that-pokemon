b26166425de73c36b49c5bc5ad2bac2ef590aa3d
Leandro Drudi <drudilea@hotmail.com>
2021-10-27 11:15:04 -0300

    Deploy settings

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:drudilea/whos-that-pokemon.git main:gh-pages
+git push -f git@github.com:drudilea/whos-that-pokemon.git master:gh-pages
 
 cd -
